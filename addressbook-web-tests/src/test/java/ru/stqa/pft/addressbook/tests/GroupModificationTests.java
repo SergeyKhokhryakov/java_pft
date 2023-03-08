5973fec33e55b482e94ac0db93980333f8f54179
SergeyKhokhryakov <hso@list.ru>
2023-03-08 18:41:54 +0300

    Collection item count control added to Group class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification (){
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Group3", "Group3_header", "Group3_footer"));
     }
@@ -16,5 +18,8 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("GroupMod", "Test1", "Description"));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
+
   }
 }
